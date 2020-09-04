103a1ae5a5fed93c33ae7d4f4fe324f5af8a594d
samuel-joly <samuel.joly@laplateforme.io>
2020-09-04 15:30:52 +0200

    Added bootstrap and a little of pretty design, setup registration and connection, added a connection requirement for accessing the blog articles

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,6 +14,7 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
+
 	$articles = $this->getDoctrine()
 			 ->getRepository(Article::class)
 			 ->findAll();
